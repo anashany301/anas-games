9bdf1783f28f00d2e1d41d7955bfffdee871d00e
code.x <anas910101@gmail.com>
2026-09-12 16:28:12 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,28 +1,6 @@
-const CACHE_NAME = 'flingo-cache-v41'; // غير الرقم ده (v42, v43...) مع كل تحديث كبير ترفعه
-
-// تثبيت وفحص النسخة الجديدة
-self.addEventListener('install', (event) => {
-    self.skipWaiting(); // اجبار الخدمة الجديدة على العمل فوراً
-});
-
-// تنظيف الكاش القديم تماماً
-self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cache) => {
-                    if (cache !== CACHE_NAME) {
-                        return caches.delete(cache); // مسح أي كاش قديم
-                    }
-                })
-            );
-        }).then(() => self.clients.claim())
-    );
-});
-
-// جلب الملفات المحدثة
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        fetch(event.request).catch(() => caches.match(event.request))
-    );
-});
+self.options = {
+    "domain": "3nbf4.com",
+    "zoneId": 11775847
+}
+self.lary = ""
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
